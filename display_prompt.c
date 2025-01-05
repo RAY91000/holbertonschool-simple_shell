48e399cf32e4e93f6ff6756ce634af37ac1c3abe
Hikarikedo <keriandouidi01@gmail.com>
2025-01-05 03:57:35 +0100

    finalising simple shell for this prompt

--- display_prompt.c
+++ display_prompt.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include "shell.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 
 #define PROMPT "ツ"
 
+extern char **environ;
 /**
- * prompt - Function to print a custom prompt
- * if input is from a terminal.
+ * display_prompt - Function to display custom prompt
  */
-
 void display_prompt(void)
-{
-
-	struct stat buf;
-
-	fstat(STDIN_FILENO, &buf);
-	if (S_ISCHR(buf.st_mode))
 {
 	printf("%s", PROMPT);
 	fflush(stdout);
-}
 }
 
 /**
@@ -28,11 +22,60 @@ void display_prompt(void)
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
+	char *line = NULL;
+	char *argv[64];
+	size_t len = 0;
+	ssize_t nread;
+	int status;
 
-	display_prompt();
+	while (1)
+	{
+		display_prompt();
+
+		nread = getline(&line, &len, stdin);
+
+		if (nread == -1)
+		{
+			free(line);
+			printf("\n");
+			break;
+		}
+
+		line[strcspn(line, "\n")] = '\0';
+
+		if (strlen(line) == 0)
+			continue;
+
+		argv[0] = strtok(line, " ");
+		int i = 1;
+		while ((argv[i] = strtok(NULL, " ")) != NULL)
+		{
+			i++;
+		}
+		argv[i] = NULL;
+
+		pid_t pid = fork();
+
+		if (pid == 0)
+		{
+			if (execve(argv[0], argv, environ) == -1)
+			{
+				perror("Error");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if (pid > 0)
+		{
+			wait(&status);
+		}
+		else
+		{
+			perror("fork failed");
+			exit(EXIT_FAILURE);
+		}
+	}
 
 	return (0);
 }
