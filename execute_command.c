6696ae2f70ea569c1ec95468c4f8dd3147ddca4e
Hikarikedo <keriandouidi01@gmail.com>
2025-01-09 18:13:12 +0100

    fixed betty

--- execute_command.c
+++ execute_command.c
@@ -12,34 +12,34 @@
  */
 int execute_command(char **argv)
 {
-    pid_t pid = fork();
-    int status;
+	pid_t pid = fork();
+	int status;
 
-    if (pid == 0)  /* Child process */
-    {
-        handle_exit(argv);  /* Check for the exit command. */
-        handle_echo(argv);  /* Check for the echo command. */
+	if (pid == 0)  /* Child process */
+	{
+		handle_exit(argv);  /* Check for the exit command. */
+		handle_echo(argv);  /* Check for the echo command. */
 
-        if (!handle_execve(argv))  /* Execute the command. */
-        {
-            exit(EXIT_FAILURE);  /* Exit with error code if execve fails */
-        }
-    }
-    else if (pid > 0)  /* Parent process */
-    {
-        wait(&status);  /* Wait for the child process to finish. */
+		if (!handle_execve(argv))  /* Execute the command. */
+		{
+			exit(EXIT_FAILURE);  /* Exit with error code if execve fails */
+		}
+	}
+	else if (pid > 0)  /* Parent process */
+	{
+		wait(&status);  /* Wait for the child process to finish. */
 
-        /* If the child process failed (exit status is non-zero), print the error */
-        if (WEXITSTATUS(status) != 0)
-        {
-            fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], argv[0]);
-        }
-    }
-    else  /* Fork failed */
-    {
-        perror("fork failed");
-        return (0);
-    }
+		/* If the child process failed (exit status is non-zero), print the error */
+		if (WEXITSTATUS(status) != 0)
+		{
+			fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], argv[0]);
+		}
+	}
+	else  /* Fork failed */
+	{
+		perror("fork failed");
+		return (0);
+	}
 
-    return (1);  /* Command executed successfully. */
+	return (1);  /* Command executed successfully. */
 }
